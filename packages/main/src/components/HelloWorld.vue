003d40e45f9ce086779e638ed101355725f975f7
Rich4st <zmingcheng204@gmail.com>
2023-02-27 21:50:13 +0800

    feat: integration vueuse & tailwind

--- packages/main/src/components/HelloWorld.vue
+++ packages/main/src/components/HelloWorld.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import { useTitle } from '@vueuse/core'
+import { ref, watchEffect } from 'vue'
 
+const title = ref('Hello World')
+
+watchEffect(() => {
+  useTitle(title.value)
+})
+
+const changeTitle = () => {
+  title.value = title.value === 'Hello Vue 3' ? 'Hello World' : 'Hello Vue 3'
+}
 </script>
 
 <template>
-  <h1>hello world</h1>
+  <h1 class=" text-9xl text-sky-400">
+    hello world
+  </h1>
+  <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" @click="changeTitle">
+    Change Title
+  </button>
 </template>
