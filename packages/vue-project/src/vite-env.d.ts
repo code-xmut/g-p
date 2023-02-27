dd27d60855da33a23ebc51e2c4133c1ce341db06
Rich4st <zmingcheng204@gmail.com>
2023-02-27 13:04:05 +0800

    first commit

---
+++ packages/vue-project/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+declare module '*.vue' {
+
+  import type { DefineComponent } from 'vue';
+
+  const vueComponent: DefineComponent<{}, {}, any>;
+
+  export default vueComponent;
+
+}
