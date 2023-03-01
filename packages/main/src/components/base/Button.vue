227cdbb6f30934d927d35d929cfaca523bcd2e16
Rich4st <zmingcheng204@gmail.com>
2023-03-01 16:59:05 +0800

    style: reset button component

--- packages/main/src/components/base/Button.vue
+++ packages/main/src/components/base/Button.vue
@@ -3,9 +3,11 @@
 withDefaults(
   defineProps<{
     text: string
+    rounded: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full'
   }>(),
   {
     text: 'Get started',
+    rounded: 'lg',
   },
 )
 </script>
@@ -14,8 +16,9 @@ withDefaults(
   <button
     v-bind="$attrs"
     type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
-    focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600
+    focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600
     dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    :class="[`rounded-${rounded}`]"
   >
     <slot v-if="$slots.icon" name="icon" />
     <span v-else>
