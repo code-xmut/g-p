6c8e3d8b8c7c7cab586e5bfc0d39180caad37abe
Rich4st <zmingcheng204@gmail.com>
2023-03-01 16:44:43 +0800

    chore: reset Navbar style

--- packages/main/src/layouts/Navbar/index.vue
+++ packages/main/src/layouts/Navbar/index.vue
@@ -1,104 +1,35 @@
 <script setup lang="ts">
-
+import { Icon } from '@iconify/vue'
+import Button from '@/components/base/Button.vue'
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-[#131219]">
+  <nav class="bg-white border-gray-200 px-2 pt-5 sm:px-4 dark:bg-[#131219] sm:pl-64">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <div class="flex items-center md:order-2">
-        <button
-          id="user-menu-button" type="button"
-          class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-          aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom"
-        >
-          <span class="sr-only">Open user menu</span>
-          <img class="w-8 h-8 rounded-full" src="@/assets/images/image2.jpg" alt="user photo">
+        <Button />
+        <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+          <Icon class="w-6 h-6" icon="mdi:menu" />
         </button>
-        <!-- Dropdown menu -->
-        <div
-          id="user-dropdown"
-          class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-        >
-          <div class="px-4 py-3">
-            <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-            <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-          </div>
-          <ul class="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Dashboard</a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Settings</a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Earnings</a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Sign
-                out</a>
-            </li>
-          </ul>
-        </div>
-        <button
-          data-collapse-toggle="mobile-menu-2" type="button"
-          class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="mobile-menu-2" aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
-      <div id="mobile-menu-2" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
         <div>
-          <form>
-            <label
-              for="default-search"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >Search</label>
-            <div class="relative">
-              <input
-                id="default-search" type="search"
-                class="block w-full min-w-[400px] p-4 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#1d1c23] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..." required
-              >
-              <button
-                type="submit"
-                class="text-white absolute right-0 bottom-0 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-8 py-4 dark:bg-[#4dc98c] dark:hover:bg-green-600 dark:focus:ring-blue-800"
-              >
-                <svg
-                  aria-hidden="true" class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </form>
+          <img class="w-10 h-10 rounded-full ml-4" src="@/assets/images/image2.jpg" alt="">
         </div>
+      </div>
+      <div id="navbar-cta" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+        <form>
+          <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div class="relative">
+            <input
+              id="default-search" type="search" class="block w-full min-w-[300px] p-3 pr-12 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50
+          dark:placeholder-gray-400 dark:text-white focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1" placeholder="Search Mockups, Logos..." required
+            >
+            <Button class="absolute -right-7 bottom-0 rounded-3xl px-6 py-3">
+              <template #icon>
+                <Icon class="w-6 h-6" icon="mdi:magnify" />
+              </template>
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   </nav>
