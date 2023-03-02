d78e978cc34c8927e6362fcd007e107b338c280a
Rich4st <zmingcheng204@gmail.com>
2023-03-02 20:20:23 +0800

    style: input component

---
+++ packages/main/src/components/base/Input.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    placeholder: string
+  }>(),
+  {
+    placeholder: 'Search...',
+  },
+)
+</script>
+
+<template>
+  <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+  <input
+    id="default-search" type="search"
+    class="block w-full min-w-[300px] p-3 pr-12 text-sm text-gray-900 border dark:border-gray-500 rounded-full bg-gray-50 dark:bg-[#1d1c23]
+    dark:placeholder-gray-400 dark:text-white focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+    :placeholder="placeholder" required
+  >
+</template>
--- packages/main/src/layouts/Navbar/index.vue
+++ packages/main/src/layouts/Navbar/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
 import Button from '@/components/base/Button.vue'
+import Input from '@/components/base/Input.vue'
 </script>
 
 <template>
@@ -17,12 +18,8 @@ import Button from '@/components/base/Button.vue'
       </div>
       <div id="navbar-cta" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
         <form>
-          <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div class="relative">
-            <input
-              id="default-search" type="search" class="block w-full min-w-[300px] p-3 pr-12 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50
-          dark:placeholder-gray-400 dark:text-white focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1" placeholder="Search Mockups, Logos..." required
-            >
+            <Input />
             <Button class="absolute -right-7 bottom-0 px-6 py-3" rounded="full">
               <template #icon>
                 <Icon class="w-6 h-6" icon="mdi:magnify" />
