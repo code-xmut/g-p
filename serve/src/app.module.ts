ccc19955b9b6945f26e5079f8a1c7e2a848ec0bf
Rich4st <zmingcheng204@gmail.com>
2023-03-03 17:03:24 +0800

    feat(serve): config mongodb

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
