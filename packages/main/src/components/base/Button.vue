c7c5d0cdc7d85e565582a5cb4d6b50fb3e9afd91
Rich4st <zmingcheng204@gmail.com>
2023-03-04 15:33:45 +0800

    style(client): set Button rounded props

--- packages/main/src/components/base/Button.vue
+++ packages/main/src/components/base/Button.vue
@@ -6,13 +6,29 @@ const props = withDefaults(
     text?: string
     disabled?: boolean
     type?: 'primary' | 'secondary' | 'danger' | 'success'
+    rounded?: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full'
   }>(),
   {
     text: '',
     disabled: false,
     type: 'primary',
+    rounded: 'lg',
   },
 )
+
+const roundedClass = computed(() => {
+  const classes = {
+    'sm': 'rounded-sm',
+    'md': 'rounded-md',
+    'lg': 'rounded-lg',
+    'xl': 'rounded-xl',
+    '2xl': 'rounded-2xl',
+    '3xl': 'rounded-3xl',
+    'full': 'rounded-full',
+  }
+
+  return classes[props.rounded]
+})
 
 const typeClass = computed(() => {
   const classes = {
@@ -28,9 +44,10 @@ const typeClass = computed(() => {
 
 <template>
   <button
-    class="px-5 py-2.5 flex justify-center items-center font-medium rounded-lg focus:ring-4
+    class="px-5 py-2.5 flex justify-center items-center font-medium focus:ring-4
     focus:outline-none focus:ring-blue-300 dark:focus:ring-gray-800"
     :class="[
+      roundedClass,
       typeClass,
       { 'opacity-50 cursor-not-allowed': disabled },
     ]"
--- packages/main/src/layouts/Navbar/index.vue
+++ packages/main/src/layouts/Navbar/index.vue
@@ -15,7 +15,7 @@ const search = () => {
   <nav class="bg-white border-gray-200 px-2 pt-5 sm:px-4 dark:bg-[#131219] sm:pl-64">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <div class="flex items-center md:order-2">
-        <Button text="Get started" />
+        <Button class="hover:bg-transparent hover:text-primary border font-medium" text="Get started" />
         <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
           <Icon class="w-6 h-6" icon="mdi:menu" />
         </button>
@@ -26,7 +26,7 @@ const search = () => {
       <div id="navbar-cta" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
         <div class="relative">
           <Input v-model:value="searchValue" />
-          <Button class="absolute -right-7 bottom-0 px-6 py-3 rounded-3xl" @click="search">
+          <Button class="absolute -right-7 bottom-0 px-6 py-3" rounded="3xl" @click="search">
             <template #icon>
               <Icon class="w-6 h-6" icon="mdi:magnify" />
             </template>
