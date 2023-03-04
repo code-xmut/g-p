47a05e362d4795fc57dcfdf3560e6e1f0272c72c
Rich4st <zmingcheng204@gmail.com>
2023-03-04 21:54:37 +0800

    feat(serve): integration swagger

--- serve/src/main.ts
+++ serve/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('capalot')
+    .setDescription('The capalot API description')
+    .setBasePath('api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(3000);
 }
