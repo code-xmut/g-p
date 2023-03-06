1c8010c5d4fee2e39dd8c7e328be4f1ee3c38274
Rich4st <zmingcheng204@gmail.com>
2023-03-07 00:05:09 +0800

    chore(c): add Link & LoginGirl Component

---
+++ packages/main/src/components/LoginGirl.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useIsMobile } from '@/composables'
+const { isMobile } = useIsMobile()
+</script>
+
+<template>
+  <div v-if="!isMobile" class="w-full h-2/5 lg:h-full lg:w-3/5 bg-darkBg mx-auto flex justify-center items-center xs:hidden">
+    <img class="w-3/5 h-3/5 block md:w-full md:h-full" src="@/assets/images/log-in-girl.svg" alt="">
+  </div>
+</template>
---
+++ packages/main/src/components/base/Link.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+export interface LinkProps {
+  text: string
+}
+
+withDefaults(defineProps<LinkProps>(), {
+  text: '',
+})
+</script>
+
+<template>
+  <a
+    v-bind="$attrs"
+    class="link text-sky-600"
+  >
+    {{ text }}
+  </a>
+</template>
