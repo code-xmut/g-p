f6ea4a99999e4c50ae0141ab4edff22cc91aa20f
Rich4st <zmingcheng204@gmail.com>
2023-03-09 13:59:38 +0800

    chore(s): remove test Module

--- serve/src/book/dto/update-book.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from '../schemas/book.schema';
-
-export class UpdateBookDto {
-  readonly title: string;
-  readonly description: string;
-  readonly author: string;
-  readonly price: number;
-  readonly category: Category;
-}
