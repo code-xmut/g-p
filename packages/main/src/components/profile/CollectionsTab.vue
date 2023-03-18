5652de9569e8a92139d6759a5c3476f17885e997
Rich4st <zmingcheng204@gmail.com>
2023-03-18 13:13:31 +0800

    chore(c): push rest file

--- packages/main/src/components/profile/CollectionsTab.vue
+++ packages/main/src/components/profile/CollectionsTab.vue
@@ -4,8 +4,8 @@
 
 <template>
   <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-6">
-    <li v-for="i in 5" :key="i">
-      <Collections />
+    <li>
+      <Collections @click="$router.push('collections')" />
     </li>
   </ul>
 </template>
