4c28038e1f7c10f93de082b8ce1d4321de190430
Rich4st <zmingcheng204@gmail.com>
2023-03-19 16:45:05 +0800

    feat(c): complete Markdown render

---
+++ packages/main/src/components/Markdown.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { marked } from 'marked'
+
+const props = defineProps<{
+  content: string
+}>()
+
+const md = props.content.replace(/\\n/g, '\n')
+const md2html = marked(md)
+</script>
+
+<template>
+  <article class="prose lg:prose-xl">
+    <div v-html="md2html" />
+  </article>
+</template>
