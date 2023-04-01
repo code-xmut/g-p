b1c4c832c050d35698c1673e847ddee970d1553b
Rich4st <zmingcheng204@gmail.com>
2023-04-01 13:25:43 +0800

    feat(c): Divider component

---
+++ packages/main/src/components/base/Divider.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  lineHeight?: string
+  lineColor?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  lineHeight: '1px',
+  lineColor: 'bg-pink-500',
+})
+
+const dividerLineHeight = computed(() => {
+  return `h-[${props.lineHeight}]`
+})
+
+const dividerLineColor = computed(() => {
+  return `${props.lineColor}`
+})
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div
+      :class="[dividerLineHeight, dividerLineColor]"
+      class="w-1/2"
+    />
+    <slot />
+    <div
+      :class="[dividerLineHeight, dividerLineColor]"
+      class="w-1/2"
+    />
+  </div>
+</template>
