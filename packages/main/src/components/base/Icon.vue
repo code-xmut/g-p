2bbee746b00823cfc6bc99807369df2bf339513e
Rich4st <zmingcheng204@gmail.com>
2023-04-01 13:25:52 +0800

    feat(c): Icon component

---
+++ packages/main/src/components/base/Icon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+interface Props {
+  icon: string
+}
+
+withDefaults(defineProps<Props>(), {
+  icon: 'mdi:plus',
+})
+</script>
+
+<template>
+  <!-- plus icon -->
+  <Icon class="w-6 h-6" :icon="icon" />
+</template>
