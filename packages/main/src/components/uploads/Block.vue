e28fbb73260249895ec6082a11cce1996f3161c5
Rich4st <zmingcheng204@gmail.com>
2023-04-01 20:40:22 +0800

    style(c): redesign Block & editor

--- packages/main/src/components/uploads/Block.vue
+++ packages/main/src/components/uploads/Block.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Block } from '@/types/editor'
 import { BlockEnum } from '@/types/editor'
+import { useEditorStore } from '@/store'
 
 interface Props {
   block: Block
@@ -8,13 +9,34 @@ interface Props {
 
 withDefaults(defineProps<Props>(), {
 })
+
+const store = useEditorStore()
 </script>
 
 <template>
-  <img v-if="block.type === BlockEnum.IMG" class="py-4" :src="block.value" alt="">
-  <textarea
-    v-else
-    :class="{ 'text-5xl': block.type === BlockEnum.H1, 'text-4xl': block.type === BlockEnum.H2 }"
-    class="w-full outline-none resize-none bg-transparent font-bold py-2" placeholder="Give me a name"
-  />
+  <div class="group">
+    <div
+      :class="{ 'lg:ml-[5vw]': store.showDrawer }"
+      class="px-2 lg:ml-[15vw]"
+    >
+      <img v-if="block.type === BlockEnum.IMG" class="py-4" :src="block.value" alt="">
+      <textarea
+        v-else
+        :class="{ 'text-5xl': block.type === BlockEnum.H1, 'text-4xl': block.type === BlockEnum.H2 }"
+        class="w-full caret-pink-500 outline-none resize-none bg-transparent font-bold py-2 group" placeholder="Give me a name"
+        :value="block.value"
+      />
+    </div>
+    <Divider line-height="4px" class="hidden group-hover:flex">
+      <Button
+        v-tip="{ content: 'Add more details, images or text', placement: 'top' }"
+        class="btn-secondary rounded-full font-normal"
+        text="insert" @click="store.toggleDrawer"
+      >
+        <template #frontIcon>
+          <Icon />
+        </template>
+      </Button>
+    </Divider>
+  </div>
 </template>
--- packages/main/src/pages/editor.vue
+++ packages/main/src/pages/editor.vue
@@ -5,20 +5,9 @@ const store = useEditorStore()
 </script>
 
 <template>
-  <div class="ml-[15vw]" :class="{ 'ml-[3vw]': store.showDrawer }">
+  <div>
     <div v-for="b in store.draft" :key="b.id">
       <Block :block="b" />
     </div>
   </div>
-  <Divider line-height="4px">
-    <Button
-      v-tip="{ content: 'Add more details, images or text', placement: 'top' }"
-      class="btn-secondary rounded-full font-normal"
-      text="insert" @click="store.toggleDrawer"
-    >
-      <template #frontIcon>
-        <Icon />
-      </template>
-    </Button>
-  </Divider>
 </template>
