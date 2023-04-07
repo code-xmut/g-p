634bcdd45bc8d5479bbe3fc33f49f207fe45fc71
Rich4st <zmingcheng204@gmail.com>
2023-04-07 17:37:14 +0800

    chore(c): push rest file

--- packages/main/src/components/profile/CollectionsTab.vue
+++ packages/main/src/components/profile/CollectionsTab.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import type { Collection } from '@gp/types'
+import { collectionsApi } from '@/api'
 
+const userId = JSON.parse(localStorage.getItem('user') || '{}')._id
+const collections = ref<Collection[]>([])
+onMounted(async () => {
+  const { data } = await collectionsApi.findUserCollections(userId)
+  collections.value = data
+})
 </script>
 
 <template>
   <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-6">
-    <li>
-      <Collections @click="$router.push('collections')" />
+    <li v-for="c in collections" :key="c._id">
+      <Collections :collection="c" @click="$router.push('collections')" />
     </li>
   </ul>
 </template>
