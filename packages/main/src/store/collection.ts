68e970f15e1f397dbfc5c1617697d3fc98ea5b19
Rich4st <zmingcheng204@gmail.com>
2023-04-08 14:29:19 +0800

    feat(c): collection store

---
+++ packages/main/src/store/collection.ts
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia'
+import { useRoute, useRouter } from 'vue-router'
+import type { Collection } from '@gp/types'
+import { collectionsApi } from '@/api'
+
+export const useCollectionStore = defineStore('collection', () => {
+  const router = useRouter()
+  const route = useRoute()
+
+  const collectionId = ref(route.query.id?.toString() || '')
+  const collection = ref<Collection | null>(null)
+  const showEditCollectionModal = ref(false)
+  const showDeleteCollectionModal = ref(false)
+
+  const getCollection = async () => {
+    if (collection.value)
+      return collection.value
+
+    const { data } = await collectionsApi.findCollectionById(collectionId.value)
+    return data
+  }
+
+  const toCollectionPage = (c: Collection) => {
+    collection.value = c
+    router.push({
+      path: '/collection',
+      query: {
+        id: c._id,
+      },
+    })
+  }
+
+  const deleteCollection = async () => {
+    const { data } = await collectionsApi.deleteCollectionById(collectionId.value)
+    if (data) {
+      router.back()
+      showDeleteCollectionModal.value = false
+    }
+  }
+
+  const updateCollectionById = async (d: Partial<Collection>) => {
+    const { data } = await collectionsApi.updateCollectionById(collectionId.value, d)
+    if (data)
+      router.go(0)
+  }
+
+  return {
+    getCollection,
+    showEditCollectionModal,
+    showDeleteCollectionModal,
+    deleteCollection,
+    updateCollectionById,
+    toCollectionPage,
+  }
+})
