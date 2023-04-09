3a8ae5475bad4fece912460a2dfbc7758cb97603
Rich4st <zmingcheng204@gmail.com>
2023-04-09 20:14:46 +0800

    feat(c): close when click modal outside

--- packages/main/src/components/Modal/index.vue
+++ packages/main/src/components/Modal/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
+
 interface Props {
   show: boolean
   noTitle?: boolean
@@ -21,9 +23,14 @@ const props = withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits(['close', 'confirm'])
 
+const target = ref<HTMLElement>()
+
 const close = () => {
   emit('close')
 }
+
+onClickOutside(target, close)
+
 const isFullScreen = computed(() => {
   return {
     'w-screen': props.fullScreen,
@@ -38,6 +45,7 @@ const isFullScreen = computed(() => {
     <input id="my-modal" type="checkbox" class="modal-toggle">
     <div v-if="show" class="modal modal-open">
       <div
+        ref="target"
         class="modal-box max-w-full lg:max-w-[50%]"
         v-bind="$attrs"
         :class="isFullScreen"
