d1c7f70a16794e554c68eef474b653cc8f3b3e61
Rich4st <zmingcheng204@gmail.com>
2023-04-11 23:37:22 +0800

    feat(c): link to likes & collections

--- packages/main/src/layouts/Navbar/SiteActionsLogged.vue
+++ packages/main/src/layouts/Navbar/SiteActionsLogged.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { useUserStore } from '@/store'
+import { useUser } from '@/composables'
 
 const store = useUserStore()
+const { userName } = useUser()
 
 const dropdownMenu = computed(() => {
   return [
@@ -16,6 +18,16 @@ const dropdownMenu = computed(() => {
     {
       name: 'Account Settings',
       link: '/setting',
+    },
+    {
+      name: 'My Likes',
+      link: `/${userName}/liked`,
+      icon: 'mdi:heart',
+    },
+    {
+      name: 'My Collections',
+      link: `/${userName}/collections`,
+      icon: 'material-symbols:create-new-folder',
     },
     {
       name: 'Sign out',
@@ -33,9 +45,14 @@ const dropdownMenu = computed(() => {
         class="ml-8" :show-status="false" size-class="w-10 h-10"
       />
       <template #content>
-        <ul v-for="d in dropdownMenu" :key="d.name" class="group px-2">
-          <li class="group-first:border-b group-first:border-gray-400 group-last:border-t group-last:border-gray-400">
+        <ul v-for="d in dropdownMenu" :key="d.name" class="group px-1 my-1">
+          <li
+            class="group-first:border-b group-[:nth-child(3)]:border-b group-first:border-gray-400
+            group-last:border-t group-last:border-gray-400"
+          >
             <RouterLink :to="d.link" @click="d.cb">
+              <Icon v-if="d.icon" class="w-6 h-6" :icon="d.icon" />
+
               {{ d.name }}
             </RouterLink>
           </li>
