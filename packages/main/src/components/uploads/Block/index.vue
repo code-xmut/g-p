b9d3de9fcac367299592c63333675b733e5ca37d
Rich4st <zmingcheng204@gmail.com>
2023-05-04 21:07:27 +0800

    feat(c): add IMG block

--- packages/main/src/components/uploads/Block/index.vue
+++ packages/main/src/components/uploads/Block/index.vue
@@ -3,6 +3,8 @@ import { useDebounceFn } from '@vueuse/core'
 import type { Block } from '@/types/editor'
 import { BlockEnum } from '@/types/editor'
 import { useEditorStore } from '@/store'
+import { unref } from 'vue'
+import { userApi } from '@/api'
 
 interface Props {
   block: Block
@@ -12,11 +14,30 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 const store = useEditorStore()
+const uploadFileRef = ref<HTMLInputElement | null>(null)
 
 const updateBlockValue = useDebounceFn((e: Event) => {
   const target = e.target as HTMLInputElement
   store.updateBlock(props.block.id, target.value)
 }, 1000)
+
+const onDraftImgClick = (block: Block) => {
+  const uploadFile = unref(uploadFileRef)
+  if (uploadFile) {
+    uploadFile.click()
+  }
+  uploadFile?.addEventListener('change', (e) => {
+    const target = e.target as HTMLInputElement
+    const file = target.files?.[0]
+    if (file) {
+      const formData = new FormData()
+      formData.append('file', file)
+      userApi.uploadFile(formData).then(({ data }) => {
+        store.updateBlock(block.id, data.url)
+      })
+    }
+  })
+}
 </script>
 
 <template>
@@ -25,7 +46,8 @@ const updateBlockValue = useDebounceFn((e: Event) => {
       class="px-2 lg:mx-[25vw]"
       :class="{ 'lg:mx-0': store.showDrawer, 'lg:ml-[10vw]': store.showDrawer, 'lg:mr-[30vw]': store.showDrawer }"
     >
-      <img v-if="block.type === BlockEnum.IMG" class="py-4" :src="block.value" alt="">
+      <input ref="uploadFileRef" hidden type="file" />
+      <img v-tip="{ content: '点击上传图片', placement: 'right'}" v-if="block.type === BlockEnum.IMG" class="py-4 cursor-pointer" :src="block.value" alt="" @click="onDraftImgClick(block)">
       <textarea
         v-else-if="block.type === BlockEnum.TEXT"
         :value="block.value" class="w-full caret-pink-500 rounded-sm outline-2 hover:outline focus:outline focus:outline-pink-500
