c50b7cc446dee54fdfc8cd8f993ef4f2a9e11b0a
Rich4st <zmingcheng204@gmail.com>
2023-05-05 19:14:59 +0800

    chore: push rest file

--- packages/main/src/components/drawer/index.vue
+++ packages/main/src/components/drawer/index.vue
@@ -22,7 +22,7 @@ onClickOutside(target, () => {
   <div
     v-if="show"
     v-bind="$attrs" ref="target"
-    class="bottom-0 w-full md:w-[20vw] rounded-tl-md rounded-tr-md shadow-2xl bg-white h-96
+    class="bottom-0 w-full md:w-[20vw] rounded-tl-md rounded-tr-md shadow-2xl bg-white dark:bg-darkBg h-96
     md:h-screen fixed z-10 px-4 py-8"
   >
     <slot />
