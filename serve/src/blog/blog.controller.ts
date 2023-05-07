02b0554daf8f31e46a466b117bbd07b060627b79
Rich4st <zmingcheng204@gmail.com>
2023-05-07 18:38:32 +0800

    feat(s): refactor create blog api

--- serve/src/blog/blog.controller.ts
+++ serve/src/blog/blog.controller.ts
@@ -14,12 +14,8 @@ import { BlogService } from './blog.service';
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
-  @Post(':author')
-  async createBlog(
-    @Param('author') author: string,
-    @Body() blog: createBlogDto,
-  ) {
-    blog.author = author;
+  @Post()
+  async createBlog(@Body() blog: createBlogDto) {
     return await this.blogService.createBlog(blog);
   }
 
