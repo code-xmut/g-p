f45a9dab635e746c64990e0287f7677184216952
Rich4st <zmingcheng204@gmail.com>
2023-05-08 21:11:30 +0800

    feat(c): add lang component

---
+++ packages/main/src/components/lang.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n();
+
+const changeLang = (lang: string) => {
+  locale.value = lang;
+};
+</script>
+
+<template>
+  <Dropdown>
+    <Icon class="pt-1.5 w-6 h-6" icon="mdi:ideogram-chinese-japanese-korean-variant" />
+    <template #content>
+      <ul>
+        <li @click="changeLang('en')">
+          <Action icon="ri:english-input" text="English" />
+        </li>
+        <li @click="changeLang('zh')">
+          <Action icon="uil:letter-chinese-a" text="中文" />
+        </li>
+      </ul>
+    </template>
+  </Dropdown>
+</template>
